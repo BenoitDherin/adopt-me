fc994ace18e97bcae81a03b31dbc45d16f76a668
Benoit Dherin <benoit.dherin@gmail.com>
2020-10-20 15:43:11 +0200

    router

--- src/useDropDown.js
+++ src/useDropDown.js
@@ -6,20 +6,20 @@ const useDropdown = (label, defaultState, options) => {
     const Dropdown = () => (
         <label htmlFor={id}>
             {label}
-            <select
+           <select
               id={id}
               value={state}
               onChange={e => setState(e.target.value)}
               onBlur={e => setState(e.target.value)}
               disabled={options.length === 0}
-              >
-                  <options>All</options>
+            >
+                  <option>All</option>
                   {options.map(item => (
                       <option key={item} value={item}>
                           {item}
                       </option>
                   ))}
-              </select>
+            </select>
         </label>
     )
     return [state, Dropdown, setState]
