89bc9d8540f3f223cdb96c77925f7d69a0f16ca7
Benoit Dherin <benoit.dherin@gmail.com>
2020-10-20 19:16:37 +0200

    add context

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import React from 'react'
 import pet from '@frontendmasters/pet'
 import Carousel from './Carousel'
 import ErrorBoundary from './ErrorBoundary'
+import ThemeContext from './ThemeContext'
 
 class Details extends React.Component{
     // constructor(props){
@@ -34,12 +35,17 @@ class Details extends React.Component{
 
         
         return (
+
             <div className="detail">
                 <Carousel media={media} />
                 <div>
                     <h1>{name}</h1>
                     <h2>{`${animal} - ${breed} - ${location}`}</h2>
-                    <button>Adopt {name}</button>
+                    <ThemeContext.Consumer>
+                        {([themeHook]) => (
+                            <button style={{ backgroundColor: themeHook }}>Adopt {name}</button>
+                        )}
+                    </ThemeContext.Consumer>
                     <p>{description}</p>
                 </div>
             </div>
