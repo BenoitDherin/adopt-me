1de9be4b272b995388d9d42841ec7d082e065b9f
Benoit Dherin <benoit.dherin@gmail.com>
2020-10-20 14:59:33 +0200

    add async submit request

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import SearchParams from './SearchParams'
 
 const App = () => {
   return (
-    <div>
-      <h1 id="someting-important">Adopt Me!</h1>
-      <SearchParams />
-    </div>
+    <React.StrictMode>
+     <div>
+       <h1 id="someting-important">Adopt Me!</h1>
+       <SearchParams />
+     </div>
+    </React.StrictMode>
   )
 }
 
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import pet, { ANIMALS } from '@frontendmasters/pet'
+import Results from './Results'
 import useDropdown from './useDropdown'
-
 
 const SearchParams = () => {
     const [location, setLocation] = useState("Seattle, WA")
     const [breeds, setBreeds] = useState([])
     const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS)
     const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds)
+    const [pets, setPets] = useState([]);
+
+    async function requestPets() {
+        const { animals } = await pet.animals({
+            location,
+            breed,
+            type: animal
+        })
+
+        setPets(animals || [])
+    }
     
     useEffect(() => {
         setBreeds([])
@@ -22,7 +33,10 @@ const SearchParams = () => {
     return (
         <div className="search-params">
             <h1>{location}</h1>
-            <form>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                requestPets()
+            }}>
                 <label htmlFor="location">
                     location
                     <input id="location" value={location} 
@@ -34,6 +48,7 @@ const SearchParams = () => {
 
                 <button>Submit</button>
             </form>
+            <Results pets={pets} />
         </div>
     )
 }
